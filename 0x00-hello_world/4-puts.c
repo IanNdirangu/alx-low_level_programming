b8e64859bc8d894d7de786107172ca05c6155cd2
IanNdirangu <puvandeerang@gmail.com>
2023-03-16 01:10:31 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
-/*
- * main -> Prints the line with puts function
- * return(0) if successful
-*/
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
+
